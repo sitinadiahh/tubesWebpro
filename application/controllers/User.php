2c0dcae807aab486a63687fd68450e1169a5c122
dellaelva <55950762+dellaelva@users.noreply.github.com>
2019-11-29 21:51:12 +0700

    commit function singup commit function logout commit function login

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -16,7 +16,10 @@ class User extends CI_Controller {
     public function index()
     {
     public function signup(){
-        $data = $this->m_user->getUserByNama($this->post('nama'));
+        $data = $this->m_user->getUserByNama($this->input->post('nama'));
+        $data = $this->m_user->getUserByEmail($this->input->post('email'));
+        $data = $this->m_user->getUserByPassword($this->input->post('password')); 
+        redirect('home_user');
     }
 
     public function login(){
@@ -29,10 +32,16 @@ class User extends CI_Controller {
             
         }
     }
-
     public function logout(){
         $this->session->sess_destroy();
         redirect('post');
+    }
+    public function comment(){
+        $data = $this->m_comment->getCommentByIdComment($this->input->post('id_comment'));
+        $data = $this->m_comment->getCommentByIdPost($this->input->post('id_post'));
+        $data = $this->m_comment->getCommentByIdUser($this->input->post('id_user'));
+        $data = $this->m_comment->getCommentByIsiComment($this->input->post('isi_comment'));
+        $data = $this->m_comment->getCommentByWaktuComment($this->input->post('waktu_comment'));
     }
 
 }
